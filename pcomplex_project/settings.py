1852f68386734f53a50ac83b1d6bc9a42c717e99
NaylsonFerreira <naylsonfsa@gmail.com>
2021-01-12 10:13:27 -0300

    configurando estrutura

--- pcomplex_project/settings.py
+++ pcomplex_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ontology_app.apps.OntologyAppConfig'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+from core_app.settings import * # noqa
