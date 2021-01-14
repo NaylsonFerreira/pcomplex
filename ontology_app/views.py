b4074ab2f8df9422ffef0de03b8a181d17b9aac7
NaylsonFerreira <naylsonfsa@gmail.com>
2021-01-14 08:58:19 -0300

    adicionando ontology_app

--- ontology_app/views.py
+++ ontology_app/views.py
@@ -1,7 +1,14 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
 # from .models import *
+from owlready2 import *
+from pcomplex_project.settings import BASE_DIR
 
 
 def index(request):
-    return HttpResponse("Ontology APP")
+    path = str(BASE_DIR)+"/ontology_app/ontologies"
+    onto_path.append(path)
+    player_profile = get_ontology("PlayerProfile.owl")
+    player_profile.load()
+    sync_reasoner()
+    return HttpResponse("player_profile")
