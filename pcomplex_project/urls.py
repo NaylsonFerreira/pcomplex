a33e559b79b124cfb9f3428ad627b115eb714503
NaylsonFerreira <naylsonfsa@gmail.com>
2021-01-30 14:27:23 -0300

    adicionando rota index

--- pcomplex_project/urls.py
+++ pcomplex_project/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core_app.urls'))
+    path('', include('core_app.urls')),
+    path('', include('ontology.urls'))
 ]
